a7e85f8fa74c8bc3e395ea002309c11ff5ddd808
Ramsey Nasser <ram@nas.sr>
2020-12-05 13:59:59 -0500

    Clean up parser testing code

--- src/parser.rs
+++ src/parser.rs
@@ -1,23 +1,22 @@
-use pest::error::*;
-use pest::Parser;
-use std::fs;
-
 #[derive(Parser, Debug)]
 #[grammar = "grammar.pest"]
 struct OskarParser;
 
-fn print_error(e: Error<Rule>, path: &str) {
-    match e.line_col {
-        LineColLocation::Pos((l, c)) => println!("{} ({}:{}:{})", e, path, l, c),
-        LineColLocation::Span((sl, sc), _) => println!("{} ({}:{}:{})", e, path, sl, sc),
-    }
-}
-
 #[cfg(test)]
 mod tests {
+    use pest::error::*;
+    use pest::Parser;
+    use std::fs;
     use super::*;
 
-    fn test_compiles(path: &str) {
+    fn print_error(e: Error<Rule>, path: &str) {
+        match e.line_col {
+            LineColLocation::Pos((l, c)) => println!("{} ({}:{}:{})", e, path, l, c),
+            LineColLocation::Span((sl, sc), _) => println!("{} ({}:{}:{})", e, path, sl, sc),
+        }
+    }
+
+    fn test_parses(path: &str) {
         let source = fs::read_to_string(path).expect("cannot read file");
         match OskarParser::parse(Rule::start, &source) {
             Err(e) => {
@@ -29,33 +28,33 @@ mod tests {
     }
     
     #[test]
-    fn test_case_01() { test_compiles("cases/test_case_01.osk"); }
+    fn test_case_01() { test_parses("cases/test_case_01.osk"); }
     #[test]
-    fn test_case_02() { test_compiles("cases/test_case_02.osk"); }
+    fn test_case_02() { test_parses("cases/test_case_02.osk"); }
     #[test]
-    fn test_case_03() { test_compiles("cases/test_case_03.osk"); }
+    fn test_case_03() { test_parses("cases/test_case_03.osk"); }
     #[test]
-    fn test_case_04_1() { test_compiles("cases/test_case_04.1.osk"); }
+    fn test_case_04_1() { test_parses("cases/test_case_04.1.osk"); }
     #[test]
-    fn test_case_04_2() { test_compiles("cases/test_case_04.2.osk"); }
+    fn test_case_04_2() { test_parses("cases/test_case_04.2.osk"); }
     #[test]
-    fn test_case_04_3() { test_compiles("cases/test_case_04.3.osk"); }
+    fn test_case_04_3() { test_parses("cases/test_case_04.3.osk"); }
     #[test]
-    fn test_case_04_5() { test_compiles("cases/test_case_04.5.osk"); }
+    fn test_case_04_5() { test_parses("cases/test_case_04.5.osk"); }
     #[test]
-    fn test_case_04_6a() { test_compiles("cases/test_case_04.6a.osk"); }
+    fn test_case_04_6a() { test_parses("cases/test_case_04.6a.osk"); }
     #[test]
-    fn test_case_04a() { test_compiles("cases/test_case_04a.osk"); }
+    fn test_case_04a() { test_parses("cases/test_case_04a.osk"); }
     #[test]
-    fn test_case_07_5a() { test_compiles("cases/test_case_07.5a.osk"); }
+    fn test_case_07_5a() { test_parses("cases/test_case_07.5a.osk"); }
     #[test]
-    fn test_case_07_5b() { test_compiles("cases/test_case_07.5b.osk"); }
+    fn test_case_07_5b() { test_parses("cases/test_case_07.5b.osk"); }
     #[test]
-    fn test_case_07_5() { test_compiles("cases/test_case_07.5.osk"); }
+    fn test_case_07_5() { test_parses("cases/test_case_07.5.osk"); }
     #[test]
-    fn test_case_07_6() { test_compiles("cases/test_case_07.6.osk"); }
+    fn test_case_07_6() { test_parses("cases/test_case_07.6.osk"); }
     #[test]
-    fn test_case_07() { test_compiles("cases/test_case_07.osk"); }
+    fn test_case_07() { test_parses("cases/test_case_07.osk"); }
     #[test]
-    fn test_case_09() { test_compiles("cases/test_case_09.osk"); }
+    fn test_case_09() { test_parses("cases/test_case_09.osk"); }
 }
